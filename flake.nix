c8d51091c23b76307ab4a1cba609c998fe39aae7
Lucas Ransan <lucas@ransan.tk>
2021-08-22 16:48:36 +0200

    switch to JSON-rpc and JWT

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          rust-analyzer
+        ];
+      };
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,68 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+use futures::future;
+use jsonrpc_core::*;
+use jsonrpc_http_server::*;
+use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
+use serde::{Deserialize, Serialize};
 
-#[macro_use] extern crate rocket;
-
-use pbkdf2::{
-    password_hash::{PasswordHash, PasswordHasher},
-    Pbkdf2
-};
-
-fn hash_password(password: String, salt: String) -> String {
-    Pbkdf2.hash_password_simple(password.as_bytes(), salt.as_ref()).unwrap().hash.unwrap().to_string()
+#[derive(Debug, Default, Clone)]
+struct Auth {
+    jwt: Option<String>,
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+impl Metadata for Auth {}
 
-#[get("/pass/<password>/<salt>")]
-fn fun(password: String, salt: String) -> String {
-    hash_password(password, salt)
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+    exp: usize,
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, fun]).launch();
+    let mut io = MetaIoHandler::default();
+
+    io.add_method("ping", |_params: Params| {
+        future::ok(Value::String("pong".to_owned()))
+    });
+
+    io.add_method_with_meta("test", |_params: Params, auth: Auth| {
+        let jwt = match auth.jwt {
+            Some(s) => s,
+            None => {
+                return future::err(Error::invalid_request());
+            }
+        };
+        let token_data = match decode::<Claims>(
+            &jwt,
+            &DecodingKey::from_secret(b"secret"),
+            &Validation::new(Algorithm::HS256),
+        ) {
+            Ok(c) => c,
+            Err(e) => {
+                return future::err(Error {
+                    code: ErrorCode::ServerError(3),
+                    message: "invalid jwt".to_owned(),
+                    data: Some(Value::String(e.to_string())),
+                });
+            }
+        };
+        future::ok(to_value(token_data.claims).unwrap())
+    });
+
+    let server = ServerBuilder::new(io)
+        .cors_allow_headers(cors::AccessControlAllowHeaders::Any)
+        .meta_extractor(|req: &hyper::Request<hyper::Body>| {
+            let jwt = req
+                .headers()
+                .get(hyper::header::AUTHORIZATION)
+                .map(|h| h.to_str().ok())
+                .flatten()
+                .map(|s| s.strip_prefix("Bearer ")) // FIXME: reliable?
+                .flatten()
+                .map(|s| s.to_owned());
+            Auth { jwt }
+        })
+        .start_http(&"127.0.0.1:3030".parse().unwrap())
+        .unwrap();
+
+    server.wait();
 }
