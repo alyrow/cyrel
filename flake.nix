92d6b0ed5249eef3a459b6cd10a27d6c5d4531ff
Lucas Ransan <lucas@ransan.tk>
2021-08-25 12:31:19 +0200

    switch to fenix

--- flake.nix
+++ flake.nix
@@ -7,20 +7,36 @@
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, fenix }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
       };
+      rust-nightly = fenix.packages.${system};
     in {
       devShell = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
-          cargo
-          rustc
-          rustfmt
-          rust-analyzer
+          (with rust-nightly; combine (with default; [
+            cargo
+            rustc
+            rust-std
+            rustfmt
+            latest.rust-src
+          ]))
+          rust-nightly.rust-analyzer
+          cargo-expand
+          sqlite
+          (diesel-cli.override {
+            sqliteSupport = true;
+            postgresqlSupport = false;
+            mysqlSupport = false;
+          })
         ];
       };
     });
