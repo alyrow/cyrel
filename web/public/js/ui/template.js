0dbc2b221b3aa6660ad0e2c470cb74d62a984cd0
alyrow <58048578+alyrow@users.noreply.github.com>
2021-08-27 17:38:32 +0200

    Templates can use forEach array's method

--- web/public/js/ui/template.js
+++ web/public/js/ui/template.js
@@ -12,6 +12,7 @@ class Template {
             if (this.readyState === 4) {
                 if (this.status === 200) {
                     let template = this.responseText;
+                    let htmlObj = document.createElement("body");
                     Object.keys(variables).forEach(key => {
                         try {
                             template = template.replace(new RegExp("\{\{(" + key + ")\}\}"), variables[key]);
@@ -19,6 +20,51 @@ class Template {
                             console.error(e);
                         }
                     });
+
+                    //// Can be a dangerous things but since we trust only files in templates folder we can say it's okay
+                    htmlObj.innerHTML = template;
+                    const elems = htmlObj.getElementsByTagName("*");
+                    for (let i = 0; i < elems.length; i++) {
+                        const elem = elems[i];
+                        const checkFor = elem.getAttribute("for-each");
+                        if (checkFor) {
+                            let evaluate = "const elementsContainer = document.createElement('div');\n";
+                            evaluate += "elementsContainer.innerHtml = ''\n";
+                            evaluate += "const " + checkFor + " = variables." + checkFor + ";\n";
+                            evaluate += checkFor + ".forEach(";
+                            const as = elem.getAttribute("as");
+                            if (as) evaluate += as + " => {\n";
+                            else evaluate += " () => {\n";
+                            const runIf = elem.getAttribute("run-if");
+                            if (runIf) evaluate += "if (" + runIf + ") {\n";
+                            let _xElement = elem.outerHTML;
+                            try {
+                                _xElement = _xElement.replace(new RegExp('for-each="\\S*"'), "");
+                            } catch (e) {
+                                console.error(e);
+                            }
+                            try {
+                                _xElement = _xElement.replace(new RegExp('as="\\S*"'), "");
+                            } catch (e) {
+                                console.error(e);
+                            }
+                            try {
+                                _xElement = _xElement.replace(new RegExp('run-if="\\S*"'), "");
+                            } catch (e) {
+                                console.error(e);
+                            }
+
+                            evaluate += "let innerHtml = _xElement;\n" +
+                                "innerHtml = innerHtml.replaceAll(/{{(\\S*)}}/gm, substring => {return eval(/{{(\\S*)}}/.exec(substring)[1])});\n" +
+                                "elementsContainer.innerHtml += innerHtml;\n";
+
+                            if (runIf) evaluate += "}\n";
+                            evaluate += "});\n" +
+                                "template = template.replace(elem.outerHTML, elementsContainer.innerHtml);";
+                            eval(evaluate);
+                        }
+                    }
+
                     element.outerHTML = template;
 
                     onLoaded();
