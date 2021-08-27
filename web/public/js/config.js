ba8627926a75bbea65d7b0b9d68f547e2b45d020
alyrow <58048578+alyrow@users.noreply.github.com>
2021-08-27 18:06:26 +0200

    Add static config manager

---
+++ web/public/js/config.js
@@ -0,0 +1,45 @@
+
+class Config {
+    /**
+     * Load a config file
+     * @type {(name: string, callback: function) => Config}
+     * @param name Name of the config
+     * @param callback Function to be called when config is loaded
+     */
+    constructor(name, callback) {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                if (this.status === 200) {
+                    Config.configs[name] = JSON.parse(this.responseText);
+                    callback(Config.configs[name]);
+                }
+                if (this.status === 404) console.error("Config not found.");
+            }
+        }
+        xhttp.open("GET", "config/" + name + ".json", true);
+        xhttp.send();
+    }
+
+    static configs = {};
+
+    /**
+     * Return an loaded config
+     * @type {(name: string) => any}
+     * @param name Name of the config
+     */
+    static getConfig(name) {
+        return this.configs[name];
+    }
+
+    /**
+     * Load a config file and call a function with the config as argument
+     * @type {(name: string, callback: function) => void}
+     * @param name Name of the config to load
+     * @param callback Function to be called when config is loaded
+     */
+    static loadConfig(name, callback) {
+        if (!this.configs[name]) new Config(name, callback);
+        else callback(this.configs[name]);
+    }
+}
