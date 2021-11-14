c3314ec0b4f0840322f16ce75540aa27f3fb88ea
Lucas Ransan <lucas@ransan.tk>
2021-11-14 20:20:51 +0100

    make nix package

--- flake.nix
+++ flake.nix
@@ -11,15 +11,35 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, fenix }:
+  outputs = { self, nixpkgs, flake-utils, fenix, naersk }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
       };
       lib = pkgs.lib;
       rust-nightly = fenix.packages.${system};
+      naersk-lib = let
+        toolchain = with rust-nightly; combine (with minimal; [
+          cargo rustc
+        ]);
+      in naersk.lib.${system}.override {
+        cargo = toolchain;
+        rustc = toolchain;
+      };
+
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+      ];
+
+      buildInputs = with pkgs; [
+        openssl
+      ];
     in {
       devShell = pkgs.mkShell {
         nativeBuildInputs = lib.singleton (with rust-nightly; combine (with default; [
@@ -30,13 +50,17 @@
           rustfmt-preview
           latest.rust-src
         ])) ++ (with pkgs; [
-          pkg-config
-          openssl
           sqlx-cli
           postgresql
           rust-nightly.rust-analyzer
           cargo-expand
-        ]);
+        ]) ++ nativeBuildInputs ++ buildInputs;
+      };
+
+      defaultPackage = naersk-lib.buildPackage {
+        src = ./.;
+        inherit nativeBuildInputs buildInputs;
+        doCheck = true;
       };
     });
 }
